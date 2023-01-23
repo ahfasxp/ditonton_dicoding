a4482238d9c8c0e50d98e27449354b5fb7d13d22
ahfasxp1 <ahfas.fazria@gmail.com>
2023-01-23 11:23:30 +0700

    fix string invalid overview

--- test/data/models/tv_response_test.dart
+++ test/data/models/tv_response_test.dart
@@ -14,7 +14,7 @@ void main() {
     backdropPath: "/rQGBjWNveVeF8f2PGRtS85w9o9r.jpg",
     voteAverage: 5.04,
     overview:
-        "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.",
+        "Based on the Pretty Little Liars series of young adult novels by Sara Shepard",
     originCountry: ["US"],
     genreIds: [18, 9648],
     originalLanguage: "en",
@@ -53,7 +53,7 @@ void main() {
             "backdrop_path": "/rQGBjWNveVeF8f2PGRtS85w9o9r.jpg",
             "vote_average": 5.04,
             "overview":
-                "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.",
+                "Based on the Pretty Little Liars series of young adult novels by Sara Shepard",
             "origin_country": ["US"],
             "genre_ids": [18, 9648],
             "original_language": "en",
